7c413a8a431a2e4734ad5645c3a7cb08e8774a14
malithkuruwita <malithdulan7@gmail.com>
2022-04-04 22:16:15 +0530

    game_details_genre section done

--- lib/ui/pages/game_details/widgets/game_details_genre_section/game_details_genre_item_section/game_details_genre_item.dart
+++ lib/ui/pages/game_details/widgets/game_details_genre_section/game_details_genre_item_section/game_details_genre_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/helper/utils.dart';
+
 import 'package:game_app/ui/pages/game_details/widgets/game_details_genre_section/game_details_genre_item_section/game_details_genre_item_image.dart';
 
 import '../../../../../../repositories/models/genre.dart';
@@ -14,12 +16,30 @@ class GameDetailsGenreItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           AspectRatio(
             aspectRatio: 1,
             child: GameDetailsGenreItemImage(imageUrl: data?.imageBackground),
           ),
-
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(Utils.shared.percentW(2)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data?.name ?? "",
+                  style: const TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15,
+                  ),
+                ),
+                Text("Games: ${data?.gamesCount ?? ""}")
+              ],
+            ),
+          )
         ],
       ),
       decoration: const BoxDecoration(
